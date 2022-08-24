b6ea5d5ad896498753a3347478749fcb4a2271ca
avokeys <alexander.ayala.palacin@gmail.com>
2022-08-24 17:48:38 +0000

    created static data for calendar.js

---
+++ src/front/js/component/calendar.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+function calendar() {
+  const programs = [
+    {
+      name: "Food Pantry",
+      description:
+        "Nutritious staples for individuals & families from 13 towns. Monthly pick-up. Brief intake required.",
+      startTime: {
+        Monday: "",
+        Tuesday: "",
+        Wednesday: "",
+        Thursday: "",
+        Friday: "",
+        Saturday: "",
+      },
+      endTime: {
+        Monday: "",
+        Tuesday: "",
+        Wednesday: "",
+        Thursday: "",
+        Friday: "",
+        Saturday: "",
+      },
+      type: "On site",
+    },
+    {
+      name: "Food Pantry",
+      description:
+        "Nutritious staples for individuals & families from 13 towns. Monthly pick-up. Brief intake required.",
+      startTime: {
+        Monday: "",
+        Tuesday: "",
+        Wednesday: "",
+        Thursday: "",
+        Friday: "",
+        Saturday: "",
+      },
+      endTime: {
+        Monday: "",
+        Tuesday: "",
+        Wednesday: "",
+        Thursday: "",
+        Friday: "",
+        Saturday: "",
+      },
+      type: "curbside",
+    },
+  ];
+  return <div>calendar</div>;
+}
+
+export default calendar;
