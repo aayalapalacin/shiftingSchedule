778bf07c615ab8033fb3bc9d8b76d21bbb00e724
holyfs <egcg1986@gmail.com>
2022-08-31 10:11:52 +0000

    added modal.js

---
+++ src/front/js/component/modal.js
@@ -0,0 +1,51 @@
+import React from "react"
+
+function Modal(props){
+  console.log(props)
+    return (
+      <>
+        <span
+          type="button"
+          className="btn btn-link"
+          data-bs-toggle="modal"
+          data-bs-target="#exampleModal"
+        >
+          Click for more info
+        </span>
+
+        <div
+          className="modal fade"
+          id="exampleModal"
+          tabIndex="-1"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLabel">
+                  {props.name}
+                </h5>
+                <button
+                  type="button"
+                  className="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div className="modal-body">{props.description}</div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  data-bs-dismiss="modal"
+                >
+                  Close
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    );}
+export default Modal;
--- src/front/js/component/programTabs.js
+++ src/front/js/component/programTabs.js
@@ -1,11 +1,69 @@
 import React from "react";
 import "../../styles/programTabs.css";
+import Modal from "../component/modal.js"
 
 function ProgramTabs() {
+  const programs = [
+    {
+      name: "Food Pantry: Onsite",
+      description:
+        "Nutritious staples for individuals & families from 13 towns. Monthly pick-up. Brief intake required.",
+      startEndTimeText: "Food Pantry Onsite: 12pm-3pm",
+      startSatText: "Food Pantry Onsite: 9am-12pm",
+      startThurText: "Food Pantry Onsite: 12pm-7pm",
+    },
+    {
+      name: "Food Pantry: Curbside",
+      description:
+        "Nutritious staples for individuals & families from 13 towns. Monthly pick-up. Brief intake required.",
+      startEndTimeText: "Food Pantry Curbside: 3pm-4pm",
+      startThurText: "Food Pantry Curbside: 5pm-7:30pm",
+      startSatText: "Food Pantry Curbside: 9am-12pm",
+    },
+    {
+      name: "Community Meals",
+      description: "To Go Hot Lunch Distributed to all.",
+      startEndTimeText: "Community Meals: 12pm-3pm",
+    },
+    {
+      name: "Mobile Food Pantry",
+      description:
+        "Located at the Boulders Apartment Homes located at 156 Brittany Drive in Amherst",
+      startEndTimeText: "Mobile Food Pantry: 1st Wed/month 1pm-2pm",
+    },
+    {
+      name: "Fresh Produce and Baked Goods",
+      description: "Distributed to all.",
+      startEndTimeText: "Fresh Produce + Baked Goods: 12pm-3pm",
+    },
+    {
+      name: "Additional Services",
+      description:
+        "Warming center, showers, laundry and lockers. **last load of laundry must be in 1.5 hours before close, last shower is 30 min. before close",
+      startEndTimeText: "Addional Services: 9am-3pm",
+    },
+  ];
+  const lista = programs.map((program, index)=>{
+    return (
+      <div className="d-inline-flex col-4 justify-content-around">
+    <div key={index} className="tab-container ms-5 me-5 p-2">
+      <div className="row"> 
+        <h2>{program.name}</h2>
+      </div>
+      <div className="row">
+        <span><Modal 
+          index={index}  
+          name={program.name}
+          description={program.description} />
+        </span>
+        </div>
+      </div>             
+    </div>
+   )
+  })
   return (
     <div>
-      {/* your code here: */}
-      ProgramTabs
+      {lista}
     </div>
   );
 }
