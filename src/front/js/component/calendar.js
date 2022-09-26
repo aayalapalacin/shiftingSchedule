64bf84b3a7c96d66610d59beebffa3c0fa4cf102
avokeys <alexander.ayala.palacin@gmail.com>
2022-09-26 01:35:31 +0000

    console logged program name

--- src/front/js/component/calendar.js
+++ src/front/js/component/calendar.js
@@ -1,10 +1,11 @@
 import React, { useEffect } from "react";
-import useStore from "../store/flux";
 import { Context } from "../store/appContext";
 import { useContext } from "react";
 function Calendar() {
   const { store, actions } = useContext(Context);
-  console.log("store", store.programs);
+  console.log("store", store.programs[1]);
+  let programObj = store.programs[1]?.name;
+  console.log(programObj);
   const programs = [
     {
       name: "Food Pantry: Onsite",
