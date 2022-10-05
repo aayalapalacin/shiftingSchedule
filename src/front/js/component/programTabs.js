ec2bd0d300a7d88717a4ca928b887cc6288b6efb
holyfs <egcg1986@gmail.com>
2022-10-05 16:07:46 +0000

    fix programTabs margin, padding, font, fontsize, bgcolor

--- src/front/js/component/programTabs.js
+++ src/front/js/component/programTabs.js
@@ -12,50 +12,49 @@ function ProgramTabs(props) {
     let tempData = [index, name, description];
     setTempData((item) => [1, ...tempData]);
   };
-
   const list = store.programs?.map((program, index) => {
     let color = "";
     if (program.name.includes("Onsite")) {
       color = { backgroundColor: "#" + 4287 + "f" + 5 };
     } else if (program.name.includes("Curbside")) {
       color = { backgroundColor: "#eb" + 4034 };
-    } else if (program.name.includes("Meals")) {
-      color = { backgroundColor: "#" + 6 + "c" + 757 + "d" };
+    } else if (program.name.includes("Additional")) {
+      color = { backgroundColor: "#F"+2770+"C" };
     } else if (program.name.includes("Mobile")) {
       color = { backgroundColor: "#" + 48 + "B" + 1 + "BA" };
-    } else if (program.name.includes("Additional")) {
-      color = { backgroundColor: "#fbb" + 442 };
     } else if (program.name.includes("Fresh")) {
-      color = { backgroundColor: "#" + 4287 + "f" + 5 };
+      color = { backgroundColor: "#fbb" + 442 };
+    } else if (program.name.includes("Meals")) {
+      color = { backgroundColor: "#"+1+"E"+53+"FA" };
     }
 
     let marginClick = "";
     if (program.name.startsWith("Fresh")) {
       marginClick = {
         position: "relative",
-        bottom: 2 + "px",
+        bottom: 2 + "px"
       };
     } else {
-      marginClick = "mt-1";
+      marginClick = "mt-3";
     }
 
     return (
       <div
-        onClick={() => {
-          getData(program.index, program.name, program.description);
-        }}
-        data-bs-toggle="modal"
-        data-bs-target="#exampleModal"
-        key={index}
-        className="d-inline-flex col-3 justify-content-around me-1">
-        <div className="tab-container mt-2 me-5 mb-3 pb-5" style={color}>
-          <div className="row">
-            <h5 className="mt-3 pe-3 ps-3" >{program.name}</h5>
+        style={color}
+        className="tab-container mt-2 me-5 mb-2 d-inline-flex justify-space-around"
+      >
+        <div
+          onClick={() => {
+            getData(program.index, program.name, program.description);
+          }}
+          data-bs-toggle="modal"
+          data-bs-target="#exampleModal"
+          key={index}
+        ><div className="row mt-2">
+          <span className="name">{program.name}</span>
           </div>
           <div className="row">
-            <span className={marginClick}>
-              <p>Click for more info</p>
-            </span>
+            <span className={marginClick}>Click for more info</span>
           </div>
         </div>
       </div>
@@ -68,7 +67,6 @@ function ProgramTabs(props) {
     </div>
   );
 }
-
 
 ProgramTabs.propTypes = {
   data: propTypes.object,
