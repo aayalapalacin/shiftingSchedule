d8e80b801e406e569c168d04b89debb35e16b111
avokeys <alexander.ayala.palacin@gmail.com>
2022-10-14 02:00:38 +0000

    validation for input start and end time 12PM format functioning

--- src/front/js/component/input.js
+++ src/front/js/component/input.js
@@ -3,14 +3,18 @@ import { Formik, Form } from "formik";
 import TextField from "./textField";
 import * as Yup from "yup";
 const validate = Yup.object({
-  programName: Yup.string()
-    .max(3, "Must be 3 characters long")
-    .required("This field is requried"),
+  programName: Yup.string().required("This field is requried"),
   programStartTime: Yup.string()
-    .max(3, "Must be 3 characters long")
+    .matches(
+      /^(?<!\S)1[0-2][AP][\M]$(?!\S)|(?<!\S)[1-9][AP][\M]$(?!\S)/,
+      "format must be correct hour followed by AM/PM all caps, no spaces"
+    )
     .required("This field is requried"),
   programEndTime: Yup.string()
-    .max(3, "Must be 3 characters long")
+    .matches(
+      /^(?<!\S)1[0-2][AP][\M]$(?!\S)|(?<!\S)[1-9][AP][\M]$(?!\S)/,
+      "format must be correct hour followed by AM/PM all caps, no spaces: 12PM, 8AM, etc"
+    )
     .required("This field is requried"),
   programDescription: Yup.string()
     .max(3, "Must be 3 characters long")
@@ -18,12 +22,24 @@ const validate = Yup.object({
   programBarTxt: Yup.string()
     .max(3, "Must be 3 characters long")
     .required("This field is requried"),
-  programThurBarTxt: Yup.string()
-    .max(3, "Must be 3 characters long")
-    .required("This field is requried"),
-  programSatBarTxt: Yup.string()
-    .max(3, "Must be 3 characters long")
-    .required("This field is requried"),
+  programThurStartTime: Yup.string().matches(
+    /^(?<!\S)1[0-2][AP][\M]$(?!\S)|(?<!\S)[1-9][AP][\M]$(?!\S)/,
+    "format must be correct hour followed by AM/PM all caps, no spaces: 12PM, 8AM, etc"
+  ),
+  programThurEndTime: Yup.string().matches(
+    /^(?<!\S)1[0-2][AP][\M]$(?!\S)|(?<!\S)[1-9][AP][\M]$(?!\S)/,
+    "format must be correct hour followed by AM/PM all caps, no spaces: 12PM, 8AM, etc"
+  ),
+  programThurBarTxt: Yup.string().max(3, "Must be 3 characters long"),
+  programSatStartTime: Yup.string().matches(
+    /^(?<!\S)1[0-2][AP][\M]$(?!\S)|(?<!\S)[1-9][AP][\M]$(?!\S)/,
+    "format must be correct hour followed by AM/PM all caps, no spaces: 12PM, 8AM, etc"
+  ),
+  programSatEndTime: Yup.string().matches(
+    /^(?<!\S)1[0-2][AP][\M]$(?!\S)|(?<!\S)[1-9][AP][\M]$(?!\S)/,
+    "format must be correct hour followed by AM/PM all caps, no spaces: 12PM, 8AM, etc"
+  ),
+  programSatBarTxt: Yup.string().max(3, "Must be 3 characters long"),
 });
 
 function Input() {
@@ -35,7 +51,11 @@ function Input() {
         programEndTime: "",
         programDescription: "",
         programBarTxt: "",
+        programThurStartTime: "",
+        programThurEndTime: "",
         programThurBarTxt: "",
+        programSatStartTime: "",
+        programSatEndTime: "",
         programSatBarTxt: "",
       }}
       validationSchema={validate}
@@ -67,8 +87,28 @@ function Input() {
               type="text"
             />
             <TextField
+              label="program thursday start time"
+              name="programThurStartTime"
+              type="text"
+            />
+            <TextField
+              label="program thursday end time"
+              name="programThurEndTime"
+              type="text"
+            />
+            <TextField
               label="program thursday bar text"
               name="programThurBarTxt"
+              type="text"
+            />
+            <TextField
+              label="program saturday start time"
+              name="programSatStartTime"
+              type="text"
+            />
+            <TextField
+              label="program saturday end time"
+              name="programSatEndTime"
               type="text"
             />
             <TextField
