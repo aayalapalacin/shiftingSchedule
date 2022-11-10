54e0365a45c84a512b0a696a7c8489105ac25afa
avokeys <alexander.ayala.palacin@gmail.com>
2022-11-10 14:02:29 +0000

    beginning custom table

--- src/front/js/pages/calendar.js
+++ src/front/js/pages/calendar.js
@@ -2,129 +2,40 @@ import React, { useEffect, useState } from "react";
 
 import "../../styles/calendar.css";
 
-function Calendar(props) {
+function Calendar() {
   return (
-    <div>
-      <div className="bg-primary col-3 PantryOnsite "></div>
-      <table className="table table-bordered ">
-        <thead>
-          <tr>
-            <th className="fontTime" scope="col">
-              <span className="text-light">9</span>9AM-10AM
-            </th>
-            <th className="fontTime" scope="col">
-              10AM-11AM
-            </th>
-            <th className="fontTime" scope="col">
-              11AM-12PM
-            </th>
-            <th className="fontTime" scope="col">
-              12PM-1PM &#8205;
-            </th>
-            <th className="fontTime" scope="col">
-              &zwnj;1PM-2PM&zwnj;
-            </th>
-            <th className="fontTime" scope="col">
-              &zwnj; 2PM-3PM &zwnj;
-            </th>
-            <th className="fontTime" scope="col">
-              &zwnj; 3PM-4PM &zwnj;
-            </th>
-            <th className="fontTime" scope="col">
-              &zwnj; 4PM-5PM &zwnj;
-            </th>
-            <th className="fontTime" scope="col">
-              &zwnj; 5PM-6PM &zwnj;
-            </th>
-            <th className="fontTime" scope="col">
-              &zwnj; 6PM-7PM &zwnj;
-            </th>
-            <th className="fontTime" scope="col">
-              &zwnj; 7PM-8PM &zwnj;
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row"></th>
-            <td className="cell-size"></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <th scope="row"></th>
-            <td className="cell-size"></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <th scope="row"></th>
-            <td className="cell-size"></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <th scope="row"></th>
-            <td className="cell-size"></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <th scope="row"></th>
-            <td className="cell-size"></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <th scope="row"></th>
-            <td className="cell-size"></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-        </tbody>
-      </table>
+    <div className="bg-warning">
+      <div className="d-flex ">
+        <div className="fontTime border border-dark px-2">09AM-10AM</div>
+        <div className="fontTime border border-dark px-2">10AM-11AM</div>
+        <div className="fontTime border border-dark px-2">11AM-12PM</div>
+        <div className="fontTime border border-dark px-2">12PM-1PM &#8205;</div>
+        <div className="fontTime border border-dark px-2">
+          &zwnj;1PM-2PM&zwnj;
+        </div>
+        <div className="fontTime border border-dark px-2">
+          &zwnj; 2PM-3PM &zwnj;
+        </div>
+        <div className="fontTime border border-dark px-2">
+          &zwnj; 3PM-4PM &zwnj;
+        </div>
+        <div className="fontTime border border-dark px-2">
+          &zwnj; 4PM-5PM &zwnj;
+        </div>
+        <div className="fontTime border border-dark px-2">
+          &zwnj; 5PM-6PM &zwnj;
+        </div>
+        <div className="fontTime border border-dark px-2">
+          &zwnj; 6PM-7PM &zwnj;
+        </div>
+        <div className="fontTime border border-dark px-2">
+          &zwnj; 7PM-8PM &zwnj;
+        </div>
+      </div>
+      <div className="d-flex">
+        <div className="border border-dark emptyCell ">&zwnj;</div>
+        <div className="border border-dark emptyCell ">&zwnj;</div>
+      </div>
     </div>
   );
 }
